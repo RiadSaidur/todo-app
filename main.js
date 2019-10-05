ebd0b1cfc26d3bc5f4d58c5900b421009339cb23
deathcrush <reidvai1998@gmail.com>
2019-10-05 12:06:06 +0600

    added feature: keyboard navigation for todos

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ window.addEventListener('load', () => {
 
   // id for todos
   // will use this for DETAILS SECTION if user selects a todo
-  let identity = 1;
+  let identity = 5;
 
   // function to add a new todo
   function addTodo(){
@@ -71,39 +71,94 @@ window.addEventListener('load', () => {
   }
 
   let current = document.querySelector('.selected');
+  const markComplete = document.querySelector('.finished');
+  const dlt = document.querySelector('.delete');
+  const description = document.querySelector('.description');
 
   function refresh(){
 
     let todos = document.querySelectorAll('.todo p');
-    let todoList = document.querySelectorAll('.todo');
+    // let todoList = document.querySelectorAll('.todo');
 
     todos.forEach(todo => {
       todo.addEventListener('click', () => {
 
-        let description = document.querySelector('.description');
         description.innerText = `Description: ${todo.innerText}`;
         document.querySelector('.status').innerText = `Status: ${todo.id}`;
 
         current.classList.remove('selected');
         todo.parentElement.parentElement.classList.add('selected');
         current = todo.parentElement.parentElement;
+        currentFocus = current.id;
 
-        document.querySelector('.finished').addEventListener('click', () => {
-          current.classList.remove('selected');
+        markComplete.addEventListener('click', () => {
           current.classList.add('complete');
-
         });
 
-        document.querySelector('.delete').addEventListener('click', () => {
+        dlt.addEventListener('click', () => {
           current.remove();
           description.innerText = `Description: `;
           document.querySelector('.status').innerText = `Status: `;
+          console.log('mouse');
         });
         
       });
     });
 
   }
+
+  let currentFocus = 0;
+  let nodes = [];
+  let length = 0;
+  let dltID = null;
+
+  //  keyboard navigation
+  document.addEventListener('keydown', e => {
+    
+    nodes = document.querySelector('.todo').children;
+    length = nodes.length - 2;
+
+    if(e.key == "ArrowDown"){
+      nodes[currentFocus].classList.remove('selected');
+      if(currentFocus >= length) currentFocus = 1;
+      else{
+        currentFocus++;
+      }
+      navigate();
+    }
+
+    else if(e.key == "ArrowUp"){
+      nodes[currentFocus].classList.remove('selected');
+      if(currentFocus <= 1) currentFocus = length;
+      else{
+        currentFocus--;
+      }
+      navigate();
+    }
+
+    function navigate(){
+      const status = nodes[currentFocus].childNodes[0].childNodes[0].id;
+      description.innerText = `Description: ${nodes[currentFocus].innerText}`;
+      document.querySelector('.status').innerText = `Status: ${status}`;
+
+      dltID = nodes[currentFocus].id;
+
+      markComplete.addEventListener('click', () => {
+        nodes[currentFocus].classList.add('complete');
+      });
+
+      dlt.addEventListener('click', () => {
+        document.getElementById(dltID).remove();
+        description.innerText = `Description: `;
+        document.querySelector('.status').innerText = `Status: `;
+        currentFocus = 1;
+      });
+
+      nodes[currentFocus].classList.add('selected');
+
+    }
+
+  });
 
   refresh();
 
